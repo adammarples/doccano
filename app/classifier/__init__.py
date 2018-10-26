f4c85109f82c00c69759da339ddf198e0a5ccc9f
adammarples <adam.marples@iprospect.com>
2018-10-26 16:08:23 +0100

    put functions in top level where they are being referenced

---
+++ app/classifier/__init__.py
@@ -0,0 +1,9 @@
+from .preprocess import build_vectorizer
+from .model import build_model
+from .utils import (
+    train_test_split,
+    load_dataset,
+    save_dataset,
+    make_output,
+)
+from .task import run
